98ca4d6aac292a19cddd9843b158c56976a2b7e1
jbeard <player69>
2021-03-05 18:32:02 -0700

    added schema.sql and seeds.sql files

---
+++ db/schema.sql
@@ -0,0 +1,45 @@
+CREATE TABLE candidates (
+  id INTEGER PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  industry_connected BOOLEAN NOT NULL
+);
+
+/* 
+
+CREATE THE DATABASE
+sqlite3 db/election.db
+
+
+POPULATE DATABASE WITH SCHEMA FILE
+sqlite3 db/election.db < db/schema.sql
+
+TO CHECK SCHEMA FILE
+.schema
+
+SEED THE TABLE USING THE FILE
+sqlite3 db/election.db < db/seeds.sql
+
+READ
+SELECT ALL ROWS FROM THE TABLE
+SELECT * FROM candidates;
+
+CREATE
+INSERT INTO candidates (first_name, last_name, industry_connected)
+VALUES
+  ('Ronald', 'Firbank', 1),
+  ('Virginia', 'Woolf', 1),
+  ('Piers', 'Gaveston', 0),
+  ('Charles', 'LeRoi', 1),
+  ('Katherine', 'Mansfield', 1),
+  ('Dora', 'Carrington', 0),
+  ('Edward', 'Bellamy', 0),
+  ('Montague', 'Summers', 1),
+  ('Octavia', 'Butler', 1),
+  ('Unica', 'Zurn', 1);
+
+
+UPDATE candidates
+SET industry_connected = 1
+WHERE id = 3;
+*/
